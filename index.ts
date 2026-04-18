69a778a9fc07bf4a5ab270a5c07510cbea96ccc9
Marc Klinger <mklinger@mklinger.de>
2026-04-18 14:36:57 +0200

    Use export-from syntax

--- index.ts
+++ index.ts
@@ -1,9 +1,3 @@
-import type { CommitAction } from "./gitlab-client";
-import { GitlabClient } from "./gitlab-client";
-import type { GitLabAuthService } from "./gitlab-auth-service";
-import { useGitLabAuth } from "./gitlab-auth-service";
-import type { GitlabCommit } from "./gitlab-types";
-
-export type { CommitAction, GitlabCommit, GitLabAuthService };
-
-export { GitlabClient, useGitLabAuth };
+export { useGitLabAuth, type GitLabAuthService } from "./gitlab-auth-service";
+export { GitlabClient, type CommitAction } from "./gitlab-client";
+export { type GitlabCommit } from "./gitlab-types";
